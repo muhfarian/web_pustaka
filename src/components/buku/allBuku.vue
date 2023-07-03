b7270f3b65ecedc0571cebfae3d5a21cd4f89d33
muhfarian <12150114744@students.uin-suska.ac.id>
2023-07-04 01:48:40 +0700

    Commit

--- src/components/buku/allBuku.vue
+++ src/components/buku/allBuku.vue
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { ref } from 'vue';
 
-const allData = 'http://web-library.000webhostapp.com/selectall_buku.php';
+const allData = 'https://web-library.000webhostapp.com/selectall_buku.php';
 const insertBuku = 'http://web-library.000webhostapp.com/insert_buku.php';
 const delBuku = 'http://web-library.000webhostapp.com/delete_buku.php';
 export default {
--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,14 +1,10 @@
 <template>
   <body class="body">
-  <div class="container">
-    <div class="child row">
+  <div class="container d-flex flex-row align-items-lg-center" style="margin-top: 10%">
       <router-link to="/allKategori" class="btn btn-success col"><h2>KATEGORI</h2></router-link>
       <router-link to="/allBuku" class="btn btn-success ms-2 col"><h2>BUKU</h2></router-link>
-    </div>
-    <div class="child col">
       <router-link to="/allAnggota" class="btn btn-success ms-2 col" ><h2>ANGGOTA</h2></router-link>
       <router-link to="allPeminjaman" class="btn btn-success ms-2 col"><h2>PEMINJAMAN</h2></router-link>
-    </div>
   </div>
 
   </body>
@@ -21,14 +17,4 @@ export default {
 </script>
 
 <style scoped>
-.container{
-  margin: 25px;
-  width: 1050px;
-}
-.child{
-  width: 500px;
-  height: 50px;
-  /* Center horizontally*/
-  margin: 0 auto;
-}
 </style>
